10f7660205c764477f6e723f734a7e5ecf931f6e
Nick <chidojiro@gmail.com>
2023-03-13 23:04:54 +0700

    fix: clean up vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import * as path from 'path';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
@@ -16,7 +15,4 @@ export default defineConfig({
     },
   },
   plugins: [react(), tsconfigPaths()],
-  resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
-  },
 });
